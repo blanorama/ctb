3b96e460d1923abb41bc12b1d2824deb96d72eaf
Eugen Blattner <eugen.blattner@mayflower.de>
2020-03-31 11:45:51 +0200

    sort command declarations

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -1,10 +1,8 @@
 <?php
-
+Artisan::add(new AddProjectTimeCommand());
+Artisan::add(new BookTimeCommand());
+Artisan::add(new ListOverviewCommand());
+Artisan::add(new ListTodayCommand());
+Artisan::add(new ProjectsCommand());
 Artisan::add(new StartWorkingtimeCommand());
 Artisan::add(new StopWorkingtimeCommand());
-Artisan::add(new BookTimeCommand());
-Artisan::add(new ListTodayCommand());
-Artisan::add(new AddProjectTimeCommand());
-Artisan::add(new ProjectsCommand());
-Artisan::add(new ListOverviewCommand());
-
