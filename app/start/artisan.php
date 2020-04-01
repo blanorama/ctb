045899d5fbca90b4b7e282b96902cde447feb98a
Eugen Blattner <eugen.blattner@mayflower.de>
2020-04-02 01:45:57 +0200

    add helper method to handle optional date arguments

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -9,3 +9,20 @@ Artisan::add(new ListTodayCommand());
 Artisan::add(new ProjectsCommand());
 Artisan::add(new StartWorkingtimeCommand());
 Artisan::add(new StopWorkingtimeCommand());
+
+/**
+ * @param $callingObject
+ * @param $dateString
+ * @return DateTime|false
+ * @throws Exception
+ */
+function handleDateArgument ($callingObject, $dateString) {
+    $date = $dateString == null ? new DateTime() : DateTime::createFromFormat('Y-m-d', $dateString);
+
+    if (!$date) {
+        $callingObject->error('[Response] Wrong format... Please use 1970-01-01 as example.');
+        exit();
+    }
+
+    return $date;
+}
