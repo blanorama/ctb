4ada387d0b9522f4a9fb3674f0a5b1f61219f61f
Eugen Blattner <eugen.blattner@mayflower.de>
2020-04-28 02:32:42 +0200

    resort commands in artisan.php
    Signed-off-by: Eugen Blattner <eugen.blattner@mayflower.de>

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::add(new AddProjectTimeCommand());
-Artisan::add(new BookTimeEndCommand());
 Artisan::add(new BookMissedPauseCommand());
 Artisan::add(new BookMissedTimeCommand());
 Artisan::add(new BookPauseCommand());
-Artisan::add(new BookTimeStartCommand());
 Artisan::add(new BookTimeCommand());
+Artisan::add(new BookTimeEndCommand());
+Artisan::add(new BookTimeStartCommand());
 Artisan::add(new ListOverviewCommand());
 Artisan::add(new ListTimeCommand());
 Artisan::add(new ProjectsCommand());
